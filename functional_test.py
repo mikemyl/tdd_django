ec72fcf4a9fc5ad82ec9b4a270a9560f64ad4015
Mike Milonakis <michael.milonakis@nokia.com>
2016-02-07 09:11:41 +0200

    Functional test check for a to-do input form

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import unittest
 
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -13,12 +15,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Check homepage
         self.browser.get('http://localhost:8000')
+
+        # Page title includes to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Check that there is an input bot (to insert a to-do item)
+        inputbox = self.browser.find_element_by_tag_name('hi').text
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Type a single to-do item and hit enter
+        inputbox.send_keys('Buy some milk')
+        input.box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy some milk' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-
-
