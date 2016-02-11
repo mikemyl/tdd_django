7ee7e45c8e712416bbb2293f1a4797270744f2b7
Mike Milonakis <michael.milonakis@nokia.com>
2016-02-11 09:55:12 +0200

    Minor refactoring on functional test

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Check homepage
         self.browser.get('http://localhost:8000')
@@ -33,21 +38,14 @@ class NewVisitorTest(unittest.TestCase):
         # Type a single to-do item and hit enter
         inputbox.send_keys('Buy some milk')
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy some milk', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy some milk')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use milk to make a cake')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy some milk', [row.text for row in rows])
-        self.assertIn(
-            '2: Use milk to make a cake', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy some milk')
+        self.check_for_row_in_list_table('2: Use milk to make a cake')
 
         self.fail('Finish the test!')
 if __name__ == '__main__':
